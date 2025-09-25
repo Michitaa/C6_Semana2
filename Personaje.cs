1e5dfb398ad81ce8a59d2d4d64184704855e9d62
Michitaa <nicolldecoll2102@gmail.com>
2025-09-25 13:07:56 -0500

    34

---
+++ Personaje.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class Personaje
+{
+    // 1 Constructor
+    public Personaje(string nombre)
+    {
+        Nombre = nombre;
+    }
+
+    // 1 Parámetro y 1 Método
+    public virtual void Atacar(Personaje objetivo)
+    {
+        Console.WriteLine($"{Nombre} ataca a {objetivo.Nombre}.");
+    }
+
+    public string Nombre { get; }
+}
+
+// Clase que Hereda de Personaje
+public class Jugador : Personaje
+{
+    // Constructor con herencia
+    public Jugador(string nombre) : base(nombre)
+    {
+    }
+
+    // Polimorfismo: sobrescribe el método Atacar
+    public override void Atacar(Personaje objetivo)
+    {
+        Console.WriteLine($"{Nombre} lanza un hechizo a {objetivo.Nombre}.");
+    }
+}
+
+// Clase de un enemigo, también hereda de Personaje
+public class Enemigo : Personaje
+{
+    // Constructor con herencia
+    public Enemigo(string nombre) : base(nombre)
+    {
+    }
+
+    // Polimorfismo: sobrescribe el método Atacar
+    public override void Atacar(Personaje objetivo)
+    {
+        Console.WriteLine($"{Nombre} gruñe y muerde a {objetivo.Nombre}.");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var heroe = new Jugador("Aragorn");
+        var orco = new Enemigo("Grúb");
+
+        // Usar los métodos polimórficos
+        heroe.Atacar(orco); // Salida: Aragorn lanza un hechizo a Grúb.
+        orco.Atacar(heroe); // Salida: Grúb gruñe y muerde a Aragorn.
+
+        Console.ReadKey();
+    }
+}
